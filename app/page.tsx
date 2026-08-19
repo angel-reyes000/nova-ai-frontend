70bb2517cec8c53a794505d268e275f44d6cac68
Angel Reyes <ar731684@gmail.com>
2026-08-19 01:48:57 -0600

    extra change in opacity

--- app/page.tsx
+++ app/page.tsx
@@ -453,7 +453,7 @@ export default function chatNovaAI() {
                 </p>
               ) : ''}
               <p className='w-full text-[0.9rem] ml-4 text-red-500 '>{errorGemini}</p>
-              <div className={'flex flex-cols items-center justify-between border border-red-400 rounded-3xl text-white w-full p-2' + (openMenu ? ' bg-[rgb(80,80,80)] opacity-60 pointer-events-none lg:pointer-events-auto lg:bg-[rgb(80,80,80)] ' : ' bg-[rgb(80,80,80)] ')}>
+              <div className={'flex flex-cols items-center justify-between border border-red-400 rounded-3xl text-white w-full p-2' + (openMenu ? ' bg-[rgb(80,80,80)] opacity-60 pointer-events-none lg:pointer-events-auto lg:bg-[rgb(80,80,80)] lg:opacity-100 ' : ' bg-[rgb(80,80,80)] ')}>
                 <div className='flex justify-center items-center w-[15%] sm:w-[10%] md:w-[7%] lg:w-[5%] py-3 px-1 rounded-4xl bg-[rgb(60,0,0)]'>
                   <FaSearch className='' />
                 </div>
